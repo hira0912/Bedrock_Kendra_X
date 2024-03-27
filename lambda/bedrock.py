08f08982aadf6c1073a9c60fc23fada9b26d7c3b
hira0912 <hira0912@gmail.com>
2024-03-27 17:38:52 +0900

    bedrock.py update

--- lambda/bedrock.py
+++ lambda/bedrock.py
@@ -1,19 +1,10 @@
 import boto3
 import json
+import os
 from botocore.config import Config
-kendra = boto3.client('kendra')
 
-my_config = Config(
-    region_name = 'us-east-1'
-)
-bedrock_runtime = boto3.client('bedrock-runtime',config=my_config)
-
-def kendra_search(event, context):
-
-    query_text = event['question']
-    
-    #Kendra Index ID ## need modified
-    index_id = 'hogefuga'  
+def kendra_search(index_id, query_text):
+    kendra = boto3.client('kendra')
 
     response = kendra.query(
         QueryText=query_text,
@@ -25,30 +16,28 @@ def kendra_search(event, context):
             },
         },
     )
-    # Kendraの応答のうち、最初から100個の結果を抽出
     results = response['ResultItems'][:100] if response['ResultItems'] else []
 
-    # 結果からドキュメントの抜粋部分のテキストを抽出
     for i in range(len(results)):
         results[i] = results[i].get("DocumentExcerpt", {}).get("Text", "").replace('\\n', ' ')
 
-    # ログ出力
     print("Received results:" + json.dumps(results, ensure_ascii=False))
     return results
-    #return json.dumps(results, ensure_ascii=False)
-
 
 def lambda_handler(event, context):
-    # プロンプトに設定する内容を取得
-    information = kendra_search(event, context)
-    
+    kendra = boto3.client('kendra')
+    my_config = Config(region_name='us-east-1')
+    bedrock_runtime = boto3.client('bedrock-runtime', config=my_config)
+
+    index_id = os.environ.get('KENDRA_INDEX_ID')
+    information = kendra_search(index_id, event['question'])
+
     information_prompts = ""
     for i in information:
         information_prompts = information_prompts + "情報:「" + i + "」\n"
-    
-    prompt = f"""
 
-Human: 
+    prompt = f"""
+H: 
 あなたはXアカウント「平目」さんのツイートを分析して説明するチャットbotです。
 以下の情報を参考に、質問について答えてください。
 
@@ -61,17 +50,13 @@ Human:
     わからない場合は正直に「わかりませんでした」と答えてください。
     また、一度Assistantの応答が終わった場合、その後新たな質問などは出力せずに終了してください。
 
-Assistant: """
-    print("プロンプト : ",prompt)
+A: """
+    print("プロンプト : ", prompt)
 
-    # モデルの設定
-    #modelId = 'anthropic.claude-3-sonnet-20240229-v1:0' #Claude3 Sonnet
-    modelId = 'anthropic.claude-3-haiku-20240307-v1:0' #Claude3 haiku
-    
+    modelId = 'anthropic.claude-3-haiku-20240307-v1:0'
     accept = 'application/json'
     contentType = 'application/json'
 
-    # リクエストBODYの指定
     body = json.dumps({
         "anthropic_version": "bedrock-2023-05-31",
         "max_tokens": 1000,
@@ -83,14 +68,12 @@ Assistant: """
         ]
     })
 
-    # Bedrock APIの呼び出し
     response = bedrock_runtime.invoke_model(
-    	modelId=modelId,
-    	accept=accept,
-    	contentType=contentType,
+        modelId=modelId,
+        accept=accept,
+        contentType=contentType,
         body=body
     )
 
-    # APIレスポンスからBODYを取り出す
     response_body = json.loads(response.get('body').read())
     return response_body
